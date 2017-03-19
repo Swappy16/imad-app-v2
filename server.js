c1403992d487c120c6eddd933ef4b8d5297b86e8
Swappy16 <swap.chhatre@gmail.com>
2017-03-19 21:30:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/article-one', function(req,res){
     res.sendFile(__dirname,'ui','article-one.html');
 });
 
-app.get('/article-one',function(req,res){
+app.get('/article-two',function(req,res){
     res.sendFile(path.join(__dirname,'ui','article-two.html'));
 });
 
