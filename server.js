7bb4282e78623daf164d2d67530b6b16c3bcc991
Swappy16 <swap.chhatre@gmail.com>
2017-03-22 12:38:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,11 +13,7 @@ var ArticleOne={
                 </p>`
 };
 
-var counter=0;
-app.get('/counter',function(req,res){
-    counter=counter+1;
-    res.send(counter.toString()); 
-});
+
 
 var app = express();
 app.use(morgan('combined'));
